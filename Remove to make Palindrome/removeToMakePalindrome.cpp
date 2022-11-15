01c74e74900a92c17e011063e90d670ab976d408
vastavtailwal <tailwalvastav@gmail.com>
2022-11-15 18:55:47 +0530

    pushed to git now

--- Remove to make Palindrome/removeToMakePalindrome.cpp
+++ Remove to make Palindrome/removeToMakePalindrome.cpp
@@ -1,9 +1,40 @@
 #include<iostream>
+#include<string>
 using namespace std;
+
+
+char removeToMakePal(string given)
+{
+    string s1 = "";
+    string s2 = "";
+    int i, j, k;
+    for(i = 0; i < given.length(); ++i)
+    {
+        for(j = given.length() - 1; j >= 0; --j)
+        {
+            if(i != j)
+            {
+                s1 = s1 + given[j];
+            }
+        }
+        for(k = s1.length() - 1; k >= 0; --k)
+        {
+            s2 = s2 + s1[k];
+        }
+        if(s1.compare(s2) == 0)
+        {
+            break;
+        }
+        s1 = "";
+        s2 = "";
+    }
+    return given[i];
+}
 
 
 int main()
 {
-    
+    string given = "1233621";
+    cout<<removeToMakePal(given);
     return 0;
 }
